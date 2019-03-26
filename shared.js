0a6747c6fcfde48f7afe3326a54f45e8f23b7a21
thiagodias63 <thidias6003@gmail.com>
2019-03-26 14:24:54 -0300

    Modulo 9, js com css

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var closeButton = document.querySelector(".modal__action--negative");
 var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 // console.dir(backdrop) // show the backdrop props that can be manipulated
 // console.dir(selectPlanButtons) // show the backdrop props that can be manipulated
 // backdrop.style.display = 'block';
@@ -13,11 +15,23 @@ for (let index = 0; index < selectPlanButtons.length; index++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 
-closeButton.addEventListener('click', closeModal);
+if (closeButton) {
+    closeButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
     backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    if (modal) {
+        modal.style.display = 'none';
+    }
 }
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
